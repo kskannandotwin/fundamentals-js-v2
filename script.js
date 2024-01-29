601e7a1f37516df93a6fcd015d00eb47e69d8e0a
kskannandotwin <kskannan.win@gmail.com>
2024-01-29 23:16:07 +0530

    29.01.2024-vav
    completed

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+
+// assignment
+let country, continent, population;
+
+country = 'India';
+continent = 'Asia';
+population = '100 Crores';
+
+console.log(country, continent, population);
