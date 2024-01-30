2170d31a5f145575db16b5f6067ceb1abe1275e5
kskannandotwin <kskannan.win@gmail.com>
2024-01-30 22:21:41 +0530

    30.01.2024-tafv
    completed

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
 
-console.log(String(23), 23);
-
-// type coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
-
-let n = "1" + 1; // '11'
-n = n - 1;
-console.log(n);
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
