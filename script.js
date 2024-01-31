d5510771855c2307eee3bc287e8e0ecdb818775a
kskannandotwin <kskannan.win@gmail.com>
2024-01-31 06:07:28 +0530

    31.01.2024-tcot
    completed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-3 + 4
-1991
-true && false && !false
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
 
-if (23 > 10) {
-  const str = '23 is bigger';
+const drink = age >= 18 ? 'wine·' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine';
+} else {
+  drink2 = 'water';
 }
+console.log(drink2);
 
-const me = 'Jonas';
-console.log(`I'm ${2037 - 1991} years old ${me}`);
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
